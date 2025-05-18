2c822c6647b0fae6b2222fca3b6f0e34e45e0b4b
bmurr3 <wdmurrayiii98@gmail.com>
2025-05-18 16:51:48 -0400

    Adding tutorial files.

---
+++ fvttnineshadows.mjs
@@ -0,0 +1,23 @@
+import { SystemActor } from "./module/documents.mjs";
+import { HeroDataModel, VillainDataModel, PawnDataModel, WeaponDataModel, SpellDataModel } from "./module/data-modules.mjs";
+
+Hooks.once("init", () => {
+    CONFIG.Actor.documentClass = SystemActor;
+
+    CONFIG.Actor.dataModels = {
+        hero: HeroDataModel,
+        villain: VillainDataModel,
+        pawn: PawnDataModel
+    }
+
+    CONFIG.Actor.trackableAttributes = {
+        hero: {
+            bar: ["resources.health", "resources.power", "goodness"],
+            value: []
+        },
+        pawn: {
+            bar: ["resources.health", "resources.power", "goodness"],
+            value: []
+        }
+    };
+});
