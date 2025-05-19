e47d612e567008dc7d706dfe2cd80f8f3a4f5e47
bmurr3 <wdmurrayiii98@gmail.com>
2025-05-18 21:19:20 -0400

    Correcting a typo.

--- build/build-packs.ts
+++ build/build-packs.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import url from "url";
 
-const asJson = ProcessingInstruction.argv[2]?.toLowerCase() === "json";
+const asJson = process.argv[2]?.toLowerCase() === "json";
 const __dirname = url.fileUrlToPath(new URL(".", import.meta.url));
 const packsDataPath = path.resolve(__dirname, "../packs");
 const packDirPaths = fs.readdirSync(packsDataPath).map((dirName) => path.resolve(__dirname, packsDataPath, dirName));
